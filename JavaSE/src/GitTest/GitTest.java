3a1040c979df131ca3d699151b82eed82b9fa0a4
RabbitSir <wll521ltt@gmail.com>
2019-03-13 21:05:37 +0800

    Git 打通测试

--- JavaSE/src/GitTest/GitTest.java
+++ JavaSE/src/GitTest/GitTest.java
@@ -2,6 +2,6 @@ package GitTest;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("Git Success!!!");
+        System.out.println("Git Success!!!!");
     }
 }
